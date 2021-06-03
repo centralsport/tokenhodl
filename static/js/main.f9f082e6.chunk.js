35190e67cd448f5c667d3e5641c5f814a75f6cc7
tokenhodl <adrianrztv@gmail.com>
2021-06-03 17:36:09 +0200

    try this

--- static/js/main.f9f082e6.chunk.js
+++ static/js/main.f9f082e6.chunk.js
@@ -3982,6 +3982,12 @@
                                                         children: "100,000 HOLDERS GIVEAWAY"
                                                     }), Object(r.jsx)("h5", {}), Object(r.jsx)("h6", {
                                                         children: " We are doing a giveaway to our first 100,000 holders.Send your tokens to our Smart Contract addres and we will send you double the amount "
+                                                    }), Object(r.jsx)("br", {}), Object(r.jsx)("a", {
+                                                        href: "https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x0E3EAF83Ea93Abe756690C62c72284943b96a6Bc",
+                                                        className: "btn-get-started scrollto dashboard",
+                                                        target: "_blank",
+                                                        children: "Buy $HODL"
+                                                    
                                                     }), t && Object(r.jsxs)("div", {
                                                         children: ["            ", Object(r.jsx)("h5", {
                                                             children: " Your Reward     Share"
@@ -3991,8 +3997,7 @@
                                                         }), Object(r.jsx)("h6", {
                                                             
                                                         })]
-                                                    }), Object(r.jsx)("h5", {}), Object(r.jsx)("h6", {
-                                                        children: " We are doing a giveaway to our first 100,000 holders.Send your tokens to our Smart Contract addres and we will send you double the amount "
+
                                                     }), Object(r.jsx)("br", {}), Object(r.jsx)("a", {
                                                         href: "https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x0E3EAF83Ea93Abe756690C62c72284943b96a6Bc",
                                                         className: "btn-get-started scrollto dashboard",
